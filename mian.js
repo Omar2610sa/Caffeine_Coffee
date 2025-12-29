b968348988c14e350c32f17f1e8762ece3cf1b47
Omar Salam <omarmooataz26@gmail.com>
2025-12-29 14:29:18 +0200

    Update: Add Filter-Buttom to Menu section

--- mian.js
+++ mian.js
@@ -1,3 +1,4 @@
+// Humberger Nav-Bar
 let navBar = document.querySelector("#navv")
 let bars = document.querySelector(".fa-bars")
 let Xbars = document.querySelector(".fa-x")
@@ -7,4 +8,29 @@ hum.addEventListener("click",()=>{
     bars.classList.toggle("active")
     Xbars.classList.toggle("active")
     navBar.classList.toggle("active")
+})
+// CLose Ul 
+document.querySelectorAll("#navv li a").forEach((link => {
+    link.addEventListener("click", ()=>{
+    navBar.classList.remove("active")
+    bars.classList.add("active")     
+    Xbars.classList.remove("active")
+    })
+}))
+// Menu Selections
+
+const filterBtn = document.querySelectorAll(".filter-btn")
+
+const menuItems = document.querySelectorAll(".item")
+filterBtn.forEach((btn)=> {
+    btn.addEventListener("click", ()=>{
+    const category = btn.getAttribute("data-category")
+    menuItems.forEach((item)=>{
+        if (category === "all" || category === item.getAttribute("data-category")){
+            item.style.display = "flex"
+        } else {
+            item.style.display = "none"
+        }
+    })
+    })
 })
